5e87a60f3b98d784140df822c4a91282961f02fd
hhhdbbs <309958697@qq.com>
2020-12-02 00:31:24 +0800

    Update WhileTree.java

--- WhileTree.java
+++ WhileTree.java
@@ -9,12 +9,18 @@ public class WhileTree {
 
     public List<Instruction> generate() {
         List<Instruction> instructions=new ArrayList<>();
-        booleanTree.setTrueOffset(1);
-        booleanTree.setJump(-(booleanTree.size()+1));
+        instructions.add(new Instruction(Operation.br,(long)0));
+
         instructions.addAll(booleanTree.getInstructions());
+
+        booleanTree.setTrueOffset(1);
         instructions.add(booleanTree.getOffset());
+
         instructions.add(new Instruction(Operation.br,(long)(booleanTree.size()-instructions.size())));
+
         instructions.addAll(booleanTree.getTrueInstructions());
+
+        booleanTree.setJump(-(booleanTree.size()+1));
         instructions.add(booleanTree.getJump());
         return instructions;
     }
